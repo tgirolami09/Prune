658174fabaa46a3e947a62e2831109350dd0043f
joachim <pjoi@passoire.fr>
2025-07-16 22:55:15 +0200

    to put more than one game per file

--- other_core/playGame.cpp
+++ other_core/playGame.cpp
@@ -1,13 +1,18 @@
 #include "GameState.hpp"
 #include "Move.hpp"
 #include <fstream>
+#include <sstream>
 #include <vector>
 
 int main(int argc, char** argv){
     string move;
     vector<Move> moves;
     ifstream file(argv[1]);
-    while(file >> move){
+    int indice = argc > 2?atoi(argv[2]):1;
+    string game;
+    for(int i=0; i<indice; i++)getline(file, game);
+    istringstream movesStr(game);
+    while(movesStr >> move){
         Move mv;
         mv.from_uci(move);
         moves.push_back(mv);
