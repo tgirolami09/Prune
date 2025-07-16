d957e0aec156c8a8c5ab89ba4694e6cad2987796
joachim <pjoi@passoire.fr>
2025-07-16 13:32:50 +0200

    to see the first state

--- other_core/playGame.cpp
+++ other_core/playGame.cpp
@@ -14,6 +14,7 @@ int main(int argc, char** argv){
     }
     GameState state;
     state.fromFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    state.print();
     for(Move mv:moves){
         state.playPartialMove(mv);
         state.print();
