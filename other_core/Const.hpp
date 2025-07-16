b8cabd5420a41f68b89684f2ee9348069bb03e72
joachim <pjoi@passoire.fr>
2025-07-16 13:32:12 +0200

    knight symbol is n

--- other_core/Const.hpp
+++ other_core/Const.hpp
@@ -17,5 +17,5 @@ const int KING=5;
 const int SPACE=6;
 const int nbPieces=6;
 map<char, int> piece_to_id = {{'r', ROOK}, {'n', KNIGHT}, {'b', BISHOP}, {'q', QUEEN}, {'k', KING}, {'p', PAWN}};
-const char id_to_piece[7] = {'p', 'k', 'b', 'r', 'q', 'k', ' '};
+const char id_to_piece[7] = {'p', 'n', 'b', 'r', 'q', 'k', ' '};
 #endif
