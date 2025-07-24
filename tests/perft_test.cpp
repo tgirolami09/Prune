d5fbb6b5e8048e975cf8920f0d678fd00ee91961
Thomas Girolami <thomas+github@girolami.org>
2025-07-25 00:07:57 +0200

    Added some tests

--- tests/perft_test.cpp
+++ tests/perft_test.cpp
@@ -1,8 +1,9 @@
 #include <gtest/gtest.h>
-#include "../other_core/BestMoveFinder.hpp"
-#include "../other_core/GameState.hpp"
+#include "../core/BestMoveFinder.hpp"
+#include "../core/GameState.hpp"
 
 const string StartFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+const string position2FromCPW = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ";
 const int alloted_space=64*1000;
 
 TEST(PerftTests, perft1) { 
@@ -32,4 +33,25 @@ TEST(PerftTests, perft4) {
     GameState game;
     game.fromFen(StartFen);
     EXPECT_EQ(perft.perft(game,4), 197281);
+}
+
+TEST(Position2FromCPW, perft1) { 
+    Perft perft(alloted_space);
+    GameState game;
+    game.fromFen(position2FromCPW);
+    EXPECT_EQ(perft.perft(game,1), 48);
+}
+
+TEST(Position2FromCPW, perft2) { 
+    Perft perft(alloted_space);
+    GameState game;
+    game.fromFen(position2FromCPW);
+    EXPECT_EQ(perft.perft(game,2), 2039);
+}
+
+TEST(Position2FromCPW, perft3) { 
+    Perft perft(alloted_space);
+    GameState game;
+    game.fromFen(position2FromCPW);
+    EXPECT_EQ(perft.perft(game,3), 97862);
 }
