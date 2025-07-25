631bc743456a47ec35ead7ec4d51feaf5e7c02c1
Thomas Girolami <thomas+github@girolami.org>
2025-07-25 11:59:13 +0200

    Added some tests

--- tests/perft_test.cpp
+++ tests/perft_test.cpp
@@ -5,6 +5,8 @@
 const string StartFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 const string position2FromCPW = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ";
 const string position3FromCPW = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
+const string position4FromCPW = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
+const string position5FromCPW = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
 const int alloted_space=64*1000;
 
 TEST(PerftTests, perft1) { 
@@ -90,4 +92,67 @@ TEST(Position3FromCPW, perft5) {
     GameState game;
     game.fromFen(position3FromCPW);
     EXPECT_EQ(perft.perft(game,5), 674624);
+}
+
+TEST(Position4FromCPW, perft1) { 
+    Perft perft(alloted_space);
+    GameState game;
+    game.fromFen(position4FromCPW);
+    EXPECT_EQ(perft.perft(game,1), 6);
+}
+
+TEST(Position4FromCPW, perft2) { 
+    Perft perft(alloted_space);
+    GameState game;
+    game.fromFen(position4FromCPW);
+    EXPECT_EQ(perft.perft(game,2), 264);
+}
+
+TEST(Position4FromCPW, perft3) { 
+    Perft perft(alloted_space);
+    GameState game;
+    game.fromFen(position4FromCPW);
+    EXPECT_EQ(perft.perft(game,3), 9467);
+}
+
+TEST(Position5FromCPW, perft1) { 
+    Perft perft(alloted_space);
+    GameState game;
+    game.fromFen(position5FromCPW);
+    EXPECT_EQ(perft.perft(game,1), 44);
+}
+
+TEST(Position5FromCPW, perft2) { 
+    Perft perft(alloted_space);
+    GameState game;
+    game.fromFen(position5FromCPW);
+    EXPECT_EQ(perft.perft(game,2), 1486);
+}
+
+TEST(Position5FromCPW, perft3) { 
+    Perft perft(alloted_space);
+    GameState game;
+    game.fromFen(position5FromCPW);
+    EXPECT_EQ(perft.perft(game,3), 62379);
+}
+
+TEST(PreventBishopFromEnPassant, perft1) {
+    Perft perft(alloted_space);
+    GameState game;
+    game.fromFen("7k/8/8/1Pp5/1K6/8/8/7B w - c6 0 2");
+    EXPECT_EQ(perft.perft(game,1), 8);
+}
+
+TEST(PreventBishopFromEnPassant, perft2) {
+    Perft perft(alloted_space);
+    GameState game;
+    game.fromFen("7k/8/8/1Pp5/1K6/8/8/7B w - c6 0 2");
+    EXPECT_EQ(perft.perft(game,2), 29);
+}
+
+TEST(PreventBishopFromEnPassant, perft3) {
+    Perft perft(alloted_space);
+    GameState game;
+    game.fromFen("7k/8/8/1Pp5/1K6/8/8/7B w - c6 0 2");
+    EXPECT_EQ(perft.perft(game,3), 369);
 }
