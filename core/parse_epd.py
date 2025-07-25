caff57eb203c807a48e81841bdac7dae82f2d4cc
joachim <pjoi@passoire.fr>
2025-07-25 23:23:56 +0200

    to test move generation from epd file

---
+++ core/parse_epd.py
@@ -0,0 +1,37 @@
+import sys
+import subprocess
+from tqdm import tqdm
+def pushCommand(prog, command):
+    prog.stdin.write(command.encode())
+    prog.stdin.flush()
+
+def readResult(prog):
+    dataMoves = {}
+    markEnd = 'Nodes searched: '
+    while 1:
+        line = prog.stdout.readline().decode('utf-8')
+        line = line.replace('\n', '')
+        if line.startswith(markEnd):
+            break
+        else:
+            L = line.split(": ")
+            if len(L) == 2 and (len(L[0]) == 4 or len(L[0]) == 5) and L[1].isdigit():
+                dataMoves[L[0]] = L[1]
+    return dataMoves, int(line[len(markEnd):])
+
+file = sys.argv[1]
+with open(file) as f:
+    listPos = f.readlines()
+prog = subprocess.Popen([sys.argv[2]], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+for pos in tqdm(listPos):
+    parts = pos.split(';')
+    fen = parts[0]
+    for tests in tqdm(parts[1:], leave=False):
+        depth, res = tests.split()
+        depth = int(depth[1:])
+        pushCommand(prog, f"position fen {fen}\n")
+        pushCommand(prog, f"go perft {depth}\n")
+        detailed, sumup = readResult(prog)
+        if int(sumup) != int(res):
+            print(fen, depth, res, sumup)
+
