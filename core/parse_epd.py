57725f1890b567ff890caf0cd9a4f453ebcb70de
joachim <pjoi@passoire.fr>
2025-07-26 11:23:30 +0200

    remove useless moves

--- core/parse_epd.py
+++ core/parse_epd.py
@@ -6,18 +6,13 @@ def pushCommand(prog, command):
     prog.stdin.flush()
 
 def readResult(prog):
-    dataMoves = {}
     markEnd = 'Nodes searched: '
     while 1:
         line = prog.stdout.readline().decode('utf-8')
         line = line.replace('\n', '')
         if line.startswith(markEnd):
             break
-        else:
-            L = line.split(": ")
-            if len(L) == 2 and (len(L[0]) == 4 or len(L[0]) == 5) and L[1].isdigit():
-                dataMoves[L[0]] = L[1]
-    return dataMoves, int(line[len(markEnd):])
+    return int(line[len(markEnd):])
 
 file = sys.argv[1]
 with open(file) as f:
@@ -33,7 +28,7 @@ for pos in tq1:
         depth = int(depth[1:])
         pushCommand(prog, f"position fen {fen}\n")
         pushCommand(prog, f"go perft {depth}\n")
-        detailed, sumup = readResult(prog)
+        sumup = readResult(prog)
         if int(sumup) != int(res):
             tq2.close()
             tq1.write(f'fen={fen} depth={depth} expected={res} res={sumup}')
