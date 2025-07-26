40e775e6dbd3bb3dde213de67a17373ba9ef030e
joachim <pjoi@passoire.fr>
2025-07-26 13:48:03 +0200

    play a game between two uci program

---
+++ core/playGame.py
@@ -0,0 +1,45 @@
+import sys
+import subprocess
+from chess import pgn
+import chess
+import time
+def pushCommand(prog, command):
+    prog.stdin.write(command.encode())
+    prog.stdin.flush()
+
+def readResult(prog):
+    dataMoves = {}
+    markEnd = 'bestmove '
+    while 1:
+        line = prog.stdout.readline().decode('utf-8')
+        line = line.replace('\n', '')
+        if line.startswith(markEnd):
+            break
+        else:
+            print(line)
+    return line[len(markEnd):].split()[0]
+
+prog1 = subprocess.Popen([sys.argv[1]], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+prog2 = subprocess.Popen([sys.argv[2]], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+movetime = int(sys.argv[3])
+game = pgn.Game()
+node = game
+if len(sys.argv) > 4:
+    board = chess.Board(sys.argv[4])
+else:
+    board = chess.Board()
+startFen = board.fen()
+moves = []
+while not board.is_game_over():
+    print(board.fen())
+    pushCommand(prog1, f"position fen {startFen} moves {" ".join(moves)}\n")
+    pushCommand(prog1, f"go movetime {movetime}\n")
+    move = readResult(prog1)
+    rmove = chess.Move.from_uci(move)
+    board.push(rmove)
+    node = node.add_variation(rmove)
+    print(board)
+    prog1, prog2 = prog2, prog1
+    moves.append(move)
+print(board.outcome())
+print(game)
