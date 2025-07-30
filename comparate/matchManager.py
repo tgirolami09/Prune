4a31b3bd56c2f4e08a19bd405f0c716c435b4833
joachim <pjoi@passoire.fr>
2025-07-30 12:27:59 +0200

    show the elo diff, and add a print to debug easly the null moves

--- comparate/matchManager.py
+++ comparate/matchManager.py
@@ -5,6 +5,7 @@ from tqdm import tqdm, trange
 from multiprocessing import Pool
 import time
 movetime = int(sys.argv[3])
+goCommand = f"go movetime {movetime}\n"
 def pushCommand(prog, command):
     prog.stdin.write(command.encode())
     prog.stdin.flush()
@@ -13,8 +14,10 @@ def readResult(prog):
     dataMoves = {}
     markEnd = 'bestmove '
     lastMate = 300
+    logs = ""
     while 1:
         line = prog.stdout.readline().decode('utf-8')
+        logs += line
         line = line.replace('\n', '')
         if line.startswith(markEnd):
             break
@@ -23,7 +26,7 @@ def readResult(prog):
                 n=int(line.split("mate ")[1].split()[0])
                 if n >= lastMate:continue
                 lastMate = n
-    return line[len(markEnd):].split()[0]
+    return line[len(markEnd):].split()[0], logs
 def playGames(args):
     id, rangeGame = args
     log = open(f"games{id}.log", "w")
@@ -40,15 +43,20 @@ def playGames(args):
             while not board.is_game_over() and not board.is_seventyfive_moves() and not board.is_fivefold_repetition():
                 pushCommand(prog, f"position fen {beginBoard} moves {" ".join(moves)}\n")
                 start = time.time()
-                pushCommand(prog, f"go movetime {movetime}\n")
+                pushCommand(prog, goCommand)
                 end = time.time()
-                move = readResult(prog)
+                move, logs = readResult(prog)
+                if(move == "h1h1"):
+                    print(f"position fen {beginBoard} moves {" ".join(moves)}\n")
+                    print(logs)
+                    print(prog.args)
+                    print(goCommand)
                 moves.append(move)
                 log.write(' '+move)
-                log.flush()
                 board.push(Move.from_uci(move))
                 _prog, prog = prog, _prog
             log.write('\n')
+            log.flush()
             winner = board.outcome().winner
             if winner is None:
                 results[2] += 1
@@ -75,3 +83,40 @@ for result in results:
     loses += result[1]
     draws += result[2]
 print(f"wins = {wins}, draws = {draws}, loses = {loses}")
+#thank to https://3dkingdoms.com/chess/elo.htm
+from math import log, sqrt, pi
+def eloDiff(percentage):
+    return -400 * log(1 / percentage - 1, 10)
+
+def inverseErrorFunction(x):
+    a = 8 * (pi - 3) / (3 * pi * (4 - pi))
+    y = log(1 - x * x)
+    z = 2 / (pi * a) + y / 2
+    ret = sqrt(sqrt(z * z - y / a) - z)
+    if (x < 0):
+        return -ret
+    return ret
+
+def phiInv(p):
+    return sqrt(2)*inverseErrorFunction(2*p-1)
+
+score = wins+draws/2
+tot = (wins+draws+loses)
+percentage = score/tot
+eloDelta = eloDiff(percentage)
+winP = wins/tot
+drawP = draws/tot
+loseP = loses/tot
+winsDev = winP*(1-percentage)**2
+drawsDev = drawP*(0.5-percentage)**2
+lossesDev = winP*(0-percentage)**2
+dev = sqrt(winsDev + drawsDev + lossesDev) / sqrt(tot)
+confidenceP = 0.95
+minConfidenceP = (1 - confidenceP) / 2
+maxConfidenceP = 1 - minConfidenceP
+stdDeviation = sqrt(winsDev + drawsDev + lossesDev) / sqrt(tot)
+devMin = percentage + phiInv(minConfidenceP) * stdDeviation
+devMax = percentage + phiInv(maxConfidenceP) * stdDeviation
+difference = eloDiff(devMax) - eloDiff(devMin)
+
+print(f"{eloDelta} +/- {difference/2}")
