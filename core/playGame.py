ef7a70ca9c40c446de91679eaa7e2884285e2021
joachim <pjoi@passoire.fr>
2025-07-30 19:35:05 +0200

    to know the number of nodes searched by program

--- core/playGame.py
+++ core/playGame.py
@@ -12,7 +12,9 @@ def readResult(prog):
     dataMoves = {}
     markEnd = 'bestmove '
     lastMate = 300
+    line = ""
     while 1:
+        lastLine = line
         line = prog.stdout.readline().decode('utf-8')
         line = line.replace('\n', '')
         if line.startswith(markEnd):
@@ -24,10 +26,12 @@ def readResult(prog):
                 if n >= lastMate:continue
                 lastMate = n
             print(line)
-    return line[len(markEnd):].split()[0]
+    nodes = int(lastLine.split("nodes ")[1].split()[0])
+    return line[len(markEnd):].split()[0], nodes
 
 prog1 = subprocess.Popen([sys.argv[1]], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 prog2 = subprocess.Popen([sys.argv[2]], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+nodes1 = nodes2 = 0
 movetime = int(sys.argv[3])
 if len(sys.argv) > 4:
     board = chess.Board(sys.argv[4] if sys.argv[4] != 'startpos' else startpos)
@@ -51,12 +55,22 @@ while not board.is_game_over() and not board.is_seventyfive_moves():
     print(board.fen())
     pushCommand(prog1, f"position fen {startFen} moves {" ".join(moves)}\n")
     pushCommand(prog1, f"go movetime {movetime}\n")
-    move = readResult(prog1)
+    move, nbNodes = readResult(prog1)
     rmove = chess.Move.from_uci(move)
     board.push(rmove)
     node = node.add_variation(rmove)
     print(board)
+    nodes1 += nbNodes
     prog1, prog2 = prog2, prog1
+    nodes1, nodes2 = nodes2, nodes1
     moves.append(move)
-print(board.outcome())
 print(game)
+winner = board.outcome().winner
+if winner == chess.WHITE:
+    print(sys.argv[1])
+elif winner == chess.BLACK:
+    print(sys.argv[2])
+else:
+    print('draw')
+print(prog1.args, nodes1)
+print(prog2.args, nodes2)
