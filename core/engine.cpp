04ef77fda4ca6c2fc819c8ace907af364791f6de
joachim <pjoi@passoire.fr>
2025-08-02 19:46:30 +0200

    to easly load the kiwipete position

--- core/engine.cpp
+++ core/engine.cpp
@@ -113,6 +113,8 @@ void doUCI(string UCI_instruction, Chess& state){
                 stream >> field;
                 fen += field+" ";
             }
+        }else if(arg == "kiwipete"){
+            fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ";
         }else{
             assert(arg == "startpos");
             fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
