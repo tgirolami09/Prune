da118c18d83aac2f3a7381b857b29b88dbc58a5b
joachim <pjoi@passoire.fr>
2025-08-12 15:15:48 +0200

    transform playGame by using the python chess library (which is more resistant to crash)

--- core/playGame.py
+++ core/playGame.py
@@ -1,40 +1,12 @@
 import sys
 import subprocess
-from chess import pgn
+from chess import pgn, engine
 import chess
 import time
 startpos = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
-def pushCommand(prog, command):
-    prog.stdin.write(command.encode())
-    prog.stdin.flush()
 
-def readResult(prog):
-    dataMoves = {}
-    markEnd = 'bestmove '
-    lastMate = 300
-    line = ""
-    while 1:
-        lastLine = line
-        line = prog.stdout.readline().decode('utf-8')
-        line = line.replace('\n', '')
-        if line.startswith(markEnd):
-            print(line)
-            break
-        elif "currmove" not in line:
-            if "mate" in line:
-                n=int(line.split("mate ")[1].split()[0])
-                if n >= lastMate:continue
-                lastMate = n
-            print(line)
-    #Because if using a book then nbNodes = 0
-    if (lastLine.count("nodes ") > 0):
-        nodes = int(lastLine.split("nodes ")[1].split()[0])
-    else:
-        nodes = -1
-    return line[len(markEnd):].split()[0], nodes
-
-prog1 = subprocess.Popen([sys.argv[1]], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-prog2 = subprocess.Popen([sys.argv[2]], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+prog1 = engine.SimpleEngine.popen_uci(sys.argv[1])
+prog2 = engine.SimpleEngine.popen_uci(sys.argv[2])
 nodes1 = nodes2 = 0
 movetime = int(sys.argv[3])
 if len(sys.argv) > 4:
@@ -44,8 +16,8 @@ if len(sys.argv) > 4:
         EloLimit = int(sys.argv[6])
         if sideLimit:prog = prog2
         else: prog = prog1
-        pushCommand(prog, "setoption name UCI_limitStrength value true\n")
-        pushCommand(prog, f"setoption name UCI_Elo value {EloLimit}\n")
+        prog.configure({'UCI_limitStrength':True})
+        prog.configure({'UCI_Elo':EloLimit})
 else:
     board = chess.Board()
 startFen = board.fen()
@@ -55,19 +27,21 @@ else:
     game = pgn.Game()
 node = game
 moves = []
+player1 = sys.argv[1]
+player2 = sys.argv[2]
 while not board.is_game_over() and not board.is_seventyfive_moves():
     print(board.fen())
-    pushCommand(prog1, f"position fen {startFen} moves {' '.join(moves)}\n")
-    pushCommand(prog1, f"go movetime {movetime}\n")
-    move, nbNodes = readResult(prog1)
-    rmove = chess.Move.from_uci(move)
+    print(' '.join(i.uci() for i in board.move_stack))
+    result = prog1.play(board, limit=engine.Limit(time=0.1), info=engine.INFO_ALL)
+    rmove = result.move
+    nbNodes = result.info['nodes']
     board.push(rmove)
     node = node.add_variation(rmove)
     print(board)
     nodes1 += nbNodes
     prog1, prog2 = prog2, prog1
     nodes1, nodes2 = nodes2, nodes1
-    moves.append(move)
+    player1, player2 = player2, player1
 game.headers["White"] = sys.argv[1]
 game.headers["Black"] = sys.argv[2]
 print(game)
@@ -78,5 +52,7 @@ elif winner == chess.BLACK:
     print(sys.argv[2])
 else:
     print('draw')
-print(prog1.args, nodes1)
-print(prog2.args, nodes2)
+print(player1, nodes1)
+print(player2, nodes2)
+prog1.quit()
+prog2.quit()
