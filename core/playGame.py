55b4f2900498cac683c32ca2cbad732b749db6c3
joachim <pjoi@passoire.fr>
2025-08-14 22:30:49 +0200

    add support for move time in addition of the normal time control

--- core/playGame.py
+++ core/playGame.py
@@ -8,19 +8,57 @@ startpos = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 prog1 = engine.SimpleEngine.popen_uci(sys.argv[1])
 prog2 = engine.SimpleEngine.popen_uci(sys.argv[2])
 nodes1 = nodes2 = 0
-#Start time in minutes
-startTime = float(sys.argv[3])
-#Time in s
-wTime = bTime = startTime * 60
-#Increment in s 
-increment = int(sys.argv[4]) 
-#small increment in ms to reduce effect of fetching data
-additionalIncrement = 20
-if len(sys.argv) > 5:
-    board = chess.Board(sys.argv[5] if sys.argv[5] != 'startpos' else startpos)
-    if(len(sys.argv) > 7):
-        sideLimit = int(sys.argv[6])
-        EloLimit = int(sys.argv[7])
+# Start time in minutes+increment in s (like 5+3 for 5 minutes and 3 seconds of increment by move)
+# or movetime in s
+timeControl = sys.argv[3]
+isMoveTime = False
+if '+' in timeControl:
+    startTime, increment = map(float, timeControl.split('+'))
+else:
+    isMoveTime = True
+    moveTime = float(timeControl)
+
+def getLimit():
+    if isMoveTime:
+        return engine.Limit(time=moveTime)
+    else:
+        return engine.Limit(white_clock=wTime, black_clock=bTime, white_inc=increment, black_inc=increment)
+
+def updateLimit(timeSpent, side):
+    if isMoveTime:return False
+    global wTime, bTime
+    #first move is not counted in time spent
+    if hasPlayedFirst[side]:
+        if side == chess.WHITE:
+            wTime -= timeSpent
+            wTime += increment+additionalIncrement/1000
+            timeRemaind = wTime
+            color = 'White'
+        else:
+            bTime -= timeSpent
+            bTime += increment+additionalIncrement/1000
+            timeRemaind = bTime
+            color = 'Black'
+        if timeRemaind > 0:
+            print(f"Prog {game.headers[color]} has {timeRemaind}s left")
+        else:
+            print(f"Player {game.headers['Black']} has no time left")
+            winnerByTime = chess.WHITE
+            return True
+    else:
+        hasPlayedFirst[side] = True
+    return False
+
+if not isMoveTime:
+    #Time in s
+    wTime = bTime = startTime * 60
+    #small increment in ms to reduce effect of fetching data
+    additionalIncrement = 20
+if len(sys.argv) > 4:
+    board = chess.Board(sys.argv[4] if sys.argv[4] != 'startpos' else startpos)
+    if(len(sys.argv) > 6):
+        sideLimit = int(sys.argv[5])
+        EloLimit = int(sys.argv[6])
         if sideLimit:prog = prog2
         else: prog = prog1
         prog.configure({'UCI_limitStrength':True})
@@ -43,41 +81,17 @@ hasPlayedFirst = [False, False]
 while not board.is_game_over() and not board.is_seventyfive_moves():
     print(board.fen())
     print(' '.join(i.uci() for i in board.move_stack))
-    timeSpent = -time.time()
-    result = prog1.play(board, limit=engine.Limit(white_clock=wTime, black_clock=bTime, white_inc=increment, black_inc=increment), info=engine.INFO_ALL)
-    timeSpent +=time.time()
+    startTime = time.time()
+    result = prog1.play(board, limit=getLimit(), info=engine.INFO_ALL)
+    endTime = time.time()
+    timeSpent = endTime-startTime
     rmove = result.move
+    print(result.info)
     #Because no nodes are visited when a book move was found
-    try:
-        nbNodes = result.info['nodes']
-        nodes1 += nbNodes
-    except:
-        print("Could not get the nb of nodes")
-
-    if (board.turn == chess.WHITE):
-        if (hasPlayedFirst[0]):
-            wTime -= timeSpent
-            if (wTime > 0):
-                wTime += increment + additionalIncrement/1000
-                print(f"Prog {game.headers['White']} has {wTime}s left")
-            else:
-                print(f"Player {game.headers['White']} has no time left")
-                winnerByTime = chess.BLACK
-                break
-        else:
-            hasPlayedFirst[0] = True
-    else:
-        if (hasPlayedFirst[1]):
-            bTime -= timeSpent
-            if (bTime > 0):
-                bTime += increment + additionalIncrement/1000
-                print(f"Prog {game.headers['Black']} has {bTime}s left")
-            else:
-                print(f"Player {game.headers['Black']} has no time left")
-                winnerByTime = chess.WHITE
-                break
-        else:
-            hasPlayedFirst[1] = True
+    nbNodes = result.info.get('nodes', 0)
+    nodes1 += nbNodes
+    if updateLimit(timeSpent, board.turn):
+        break
 
     board.push(rmove)
     node = node.add_variation(rmove)
