7aed9ba15ed865ba303b8651a8b0500da0e80b75
joachim <pjoi@passoire.fr>
2025-12-06 22:29:03 +0100

    close zip file

--- nnue/zipmanager.cpp
+++ nnue/zipmanager.cpp
@@ -1,30 +1,37 @@
-#include <string>
 #include <zip.h>
-#include <zipconf.h>
-using namespace std;
 const int dataSize = 12*64*2+2*4;
 zip_t** zip;
+int numzips;
 extern "C" {
 void allocate_zips(int nbZips){
     zip = new zip_t*[nbZips];
+    numzips = nbZips;
 }
 
 void open_zip(char* filename, int id){
     int error;
-    zip[id] = zip_open(filename, ZIP_FL_OVERWRITE|ZIP_FL_ENC_UTF_8, &error);
+    zip[id] = zip_open(filename, ZIP_CREATE, &error);
 }
 
 void add_data(int idZip, int id, char* data){
     zip_source_t* source = zip_source_buffer(zip[idZip], data, dataSize, 0);
-    zip_file_add(zip[idZip], to_string(id).c_str(), source, ZIP_FL_OVERWRITE|ZIP_FL_COMPRESSED);
+    char snum[5];
+    sprintf(snum, "%4x", id);
+    zip_file_add(zip[idZip], snum, source, ZIP_FL_OVERWRITE|ZIP_FL_COMPRESSED);
 }
 
 void read_data(int idZip, char* buffer, int id){
-    zip_uint64_t index = zip_name_locate(zip[idZip], to_string(id).c_str(), 0);
+    char snum[5];
+    zip_uint64_t index = zip_name_locate(zip[idZip], snum, 0);
     zip_file_t* zip_file = zip_fopen_index(zip[idZip], index, 0);
     zip_uint64_t num_read = zip_fread(zip_file, buffer, dataSize);
 }
 int get_nb_files(int idZip){
     return zip_get_num_entries(zip[idZip], 0);
 }
+
+void clear_zip(int idZip){
+    zip_close(zip[idZip]);
+}
+
 }
