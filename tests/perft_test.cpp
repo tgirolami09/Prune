7676cb531c44abcccf41d32d2717a5ea557383cf
joachim <pjoi@passoire.fr>
2025-12-20 16:03:10 +0100

    because there is no more hash for perft

--- tests/perft_test.cpp
+++ tests/perft_test.cpp
@@ -1,3 +1,4 @@
+int nbThreads = 1;
 #include <gtest/gtest.h>
 #include "../core/BestMoveFinder.cpp"
 #include "../core/GameState.cpp"
@@ -22,165 +23,165 @@ const int alloted_space=64*1000;
 
 TEST(PerftTests, perft1) { 
     
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(StartFen);
     EXPECT_EQ(perft.perft(game,1), 20);
 }
 
 TEST(PerftTests, perft2) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(StartFen);
     EXPECT_EQ(perft.perft(game,2), 400);
 }
 
 TEST(PerftTests, perft3) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(StartFen);
     EXPECT_EQ(perft.perft(game,3), 8902);
 }
 
 TEST(PerftTests, perft4) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(StartFen);
     EXPECT_EQ(perft.perft(game,4), 197281);
 }
 
 TEST(Position2FromCPW, perft1) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(position2FromCPW);
     EXPECT_EQ(perft.perft(game,1), 48);
 }
 
 TEST(Position2FromCPW, perft2) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(position2FromCPW);
     EXPECT_EQ(perft.perft(game,2), 2039);
 }
 
 TEST(Position2FromCPW, perft3) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(position2FromCPW);
     EXPECT_EQ(perft.perft(game,3), 97862);
 }
 
 TEST(Position3FromCPW, perft1) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(position3FromCPW);
     EXPECT_EQ(perft.perft(game,1), 14);
 }
 
 TEST(Position3FromCPW, perft2) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(position3FromCPW);
     EXPECT_EQ(perft.perft(game,2), 191);
 }
 
 TEST(Position3FromCPW, perft3) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(position3FromCPW);
     EXPECT_EQ(perft.perft(game,3), 2812);
 }
 
 TEST(Position3FromCPW, perft4) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(position3FromCPW);
     EXPECT_EQ(perft.perft(game,4), 43238);
 }
 
 TEST(Position3FromCPW, perft5) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(position3FromCPW);
     EXPECT_EQ(perft.perft(game,5), 674624);
 }
 
 TEST(Position4FromCPW, perft1) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(position4FromCPW);
     EXPECT_EQ(perft.perft(game,1), 6);
 }
 
 TEST(Position4FromCPW, perft2) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(position4FromCPW);
     EXPECT_EQ(perft.perft(game,2), 264);
 }
 
 TEST(Position4FromCPW, perft3) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(position4FromCPW);
     EXPECT_EQ(perft.perft(game,3), 9467);
 }
 
 TEST(Position5FromCPW, perft1) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(position5FromCPW);
     EXPECT_EQ(perft.perft(game,1), 44);
 }
 
 TEST(Position5FromCPW, perft2) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(position5FromCPW);
     EXPECT_EQ(perft.perft(game,2), 1486);
 }
 
 TEST(Position5FromCPW, perft3) { 
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen(position5FromCPW);
     EXPECT_EQ(perft.perft(game,3), 62379);
 }
 
 TEST(PreventBishopFromEnPassant, perft1) {
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen("7k/8/8/1Pp5/1K6/8/8/7B w - c6 0 2");
     EXPECT_EQ(perft.perft(game,1), 8);
 }
 
 TEST(PreventBishopFromEnPassant, perft2) {
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen("7k/8/8/1Pp5/1K6/8/8/7B w - c6 0 2");
     EXPECT_EQ(perft.perft(game,2), 29);
 }
 
 TEST(PreventBishopFromEnPassant, perft3) {
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen("7k/8/8/1Pp5/1K6/8/8/7B w - c6 0 2");
     EXPECT_EQ(perft.perft(game,3), 369);
 }
 TEST(EnPassantInRay, perft2){
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen("rnbqk1nr/pppp1pbp/6p1/4P3/8/2K5/PPP1PPPP/RNBQ1BNR b kq - 0 4");
     EXPECT_EQ(perft.perft(game, 2), 912);
 }
 TEST(EnPassantNothing, perft1){
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen("8/2p5/3p4/KP5r/4Ppk1/8/6P1/7R b - e3 0 3");
     EXPECT_EQ(perft.perft(game, 1), 20);
 }
 TEST(EnPassantLogicTest, perft6){
-    Perft perft(alloted_space);
+    Perft perft;
     GameState game;
     game.fromFen("8/p2n1p2/1p1Pp2p/4P1k1/r4p1P/5K2/6P1/4R3 b - - 0 1");
     EXPECT_EQ(perft.perft(game, 6), 5076659);
