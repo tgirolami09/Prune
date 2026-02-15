f76a7b75ed7f9dcb4a77e4e41fd18728d6ded952
joachim <pjoi@passoire.fr>
2026-02-15 18:39:13 +0100

    yoinked from stash

--- core/BestMoveFinder.cpp
+++ core/BestMoveFinder.cpp
@@ -622,7 +622,7 @@ bestMoveResponse BestMoveFinder::iterativeDeepening(usefull& ss, GameState& stat
             }
             if(running)
                 allInfos.push_back({ss.nodes, (int)(tcpu*1000), (int)(speed), depth, ss.seldepth-startRelDepth, bestScore});
-            softBoundTime = chrono::milliseconds{tm.updateSoft(ss.bestMoveNodes, lastUsedNodes, parameters, verbose)};
+            softBoundTime = chrono::milliseconds{tm.updateSoft(ss.bestMoveNodes, lastUsedNodes, bestMove.moveInfo, parameters, verbose)};
             this->hardBound = tm.hardBound;
             hardBoundTime = chrono::milliseconds{tm.hardBound};
             if(limitWay == 1 && ss.nodes > tm.softBound)break;
