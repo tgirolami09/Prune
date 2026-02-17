26c65ff6f0cea025c38b5dd99b7889f83b43cf82
joachim <pjoi@passoire.fr>
2026-02-17 16:42:05 +0100

    try without the extension

--- core/BestMoveFinder.cpp
+++ core/BestMoveFinder.cpp
@@ -319,7 +319,6 @@ int BestMoveFinder::negamax(usefull& ss, int depth, GameState& state, int alpha,
                     return razscore;
                 }
                 ss.generator.initDangers(state);
-                depth++;
             }
             int r = (depth*parameters.nmp_red_depth_div+parameters.nmp_red_base)/1024;
             if(rootDist >= ss.min_nmp_ply && depth >= r && !ss.eval.isOnlyPawns() && static_eval >= beta){
