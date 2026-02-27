04c48f7f501e6275b0ea2c0dee92ff05680bc0ff
joachim <pjoi@passoire.fr>
2026-02-27 22:11:06 +0100

    factorise

--- core/simd_definitions.hpp
+++ core/simd_definitions.hpp
@@ -16,29 +16,24 @@
 // Manual SIMD wrapper for cross-platform compatibility
 #ifdef __AVX512F__
     using simd16 = __m512i;
-    const int nb16=32;
     using simdint = __m512i;
-    const int nbint=16;
     #define MM _mm512
     #define SIZE 512
 #elif defined(__AVX2__)
-    using simd16 = __m256i;  // 16 int16_t values
-    const int nb16 = 16;
+    using simd16 = __m256i; 
     using simdint = __m256i;
-    const int nbint = 8;  // 16 int32 values total
     #define MM _mm256
     #define SIZE 256
 #elif defined(__SSE2__)
-    using simd16 = __m128i;  // 8 int16_t values  
-    const int nb16 = 8;
-    // For SSE2, we need TWO __m128i to hold 8 int32 values
+    using simd16 = __m128i;
     using simdint = __m128i;
-    const int nbint = 4;  // 8 int32 values total
     #define MM _mm
     #define SIZE 128
 #else
     #error "This code requires at least SSE2 support"
 #endif
+const int nb16 = sizeof(simd16)/sizeof(int16_t);
+const int nbint = sizeof(simdint)/sizeof(int32_t);
 #define CONCAT2(a, b) a ## b
 #define CONCAT(a, b) CONCAT2(a, b)
 #define ADDMM(func_name) CONCAT(MM, _ ## func_name)
