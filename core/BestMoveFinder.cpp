f9f51787e85ba1e818d39033431648592b421c7d
joachim <pjoi@passoire.fr>
2026-04-07 20:55:55 +0200

    tweak it a bit

--- core/BestMoveFinder.cpp
+++ core/BestMoveFinder.cpp
@@ -482,8 +482,8 @@ int BestMoveFinder::negamax(usefull& ss, int depth, GameState& state, int alpha,
                 if(!isPV && moveHistory < -parameters.mchp_mul*depth*depth && depth <= 4)
                     continue;
             }
-            int margin = curMove.isTactical() ? 80*depth : 60*depth*depth;
-            if((!(flag&1) || !curMove.isTactical()) && see_ge(bbs, margin, curMove, state, value_pieces))
+            int margin = curMove.isTactical() ? 100*depth : 80*depth*depth;
+            if(!isPV && (!(flag&1) || !curMove.isTactical()) && see_ge(bbs, margin, curMove, state, value_pieces))
                 continue;
         }
 #ifdef DEBUG_MACRO
