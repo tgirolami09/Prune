ac86945ab23305be6a4d06009dfc3506f5681773
joachim <pjoi@passoire.fr>
2026-04-13 10:22:22 +0200

    no rfp in hindsight

--- core/BestMoveFinder.cpp
+++ core/BestMoveFinder.cpp
@@ -373,6 +373,7 @@ int BestMoveFinder::negamax(usefull& ss, int depth, GameState& state, int alpha,
                 margin = parameters.rfp_nimproving*depth;
             if(expected_score >= beta+margin)
                 return expected_score;
+            }
             int r = (depth*parameters.nmp_red_depth_div+parameters.nmp_red_base)/1024;
             if(rootDist >= ss.min_nmp_ply && depth >= r && !ss.eval.isOnlyPawns() && static_eval >= beta){
                 ss.stack[rootDist].snap.save(state);
@@ -406,7 +407,6 @@ int BestMoveFinder::negamax(usefull& ss, int depth, GameState& state, int alpha,
                     };
                 };
                 ss.generator.initDangers(state);
-            }
             }
         }
     }
