14e027736a426eafd03284e93237faec479dd355
joachim <pjoi@passoire.fr>
2026-07-17 10:55:52 +0200

    forgot alignas

--- core/embeder.hpp
+++ core/embeder.hpp
@@ -1,8 +1,6 @@
 #define BINARY_INCLUDE(buffername) \
 extern "C"{\
     extern const unsigned char buffername[]; \
-    extern const unsigned char* buffername##_end; \
-    extern const int buffername##_size; \
 }
 
 BINARY_INCLUDE(magicsData);
