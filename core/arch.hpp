0e5092af4bbb19c6b5206b632547bcfed4adbb19
joachim <pjoi@passoire.fr>
2026-08-23 10:37:36 +0200

    separate in multiple files

---
+++ core/arch.hpp
@@ -0,0 +1,43 @@
+#pragma once
+const int INPUT_SIZE = 11*64; //merged king planes
+const int THREAT_SIZE = 60144;
+
+constexpr int QA = 255;
+constexpr int QB = 128;
+constexpr int QC = 64;
+constexpr int FT_BITS = 9;
+constexpr int FT_LSHIFT = 16-FT_BITS;
+
+static constexpr inline int ilog2c(int n){
+    return (31^__builtin_clz(n))+!!(n&(n-1));
+}
+
+static constexpr inline int _abs(int x){
+    return x < 0?-x:x;
+}
+
+constexpr int QA_bits = ilog2c(QA);
+constexpr int QB_bits = ilog2c(QB);
+constexpr int QC_bits = ilog2c(QC);
+constexpr int L1shift = _abs(16+QC_bits-FT_LSHIFT-QA_bits*2-QB_bits);
+
+const int BUCKET = 8;
+const int nbInputBuckets = 16;
+
+const int L1 = 640;
+const int L2=16;
+const int L3=32;
+
+const int SCALE = 283;
+const int inputBuckets[32] = {
+    0 , 1 , 2 , 3 ,
+    4 , 5 , 6 , 7 ,
+    8 , 8 , 9 , 9 ,
+    10, 10, 11, 11,
+    12, 12, 13, 13,
+    12, 12, 13, 13,
+    14, 14, 15, 15,
+    14, 14, 15, 15,
+
+};
+const int DIVISOR=32/BUCKET;
--- core/simd_definitions.hpp
+++ core/simd_definitions.hpp
@@ -111,6 +111,17 @@ inline simd<16> simd16_set1(dbyte value){
 inline simd<32> simdint_set1(int value){
     return ADDMM(set1_epi32)(value);
 }
+inline uint32_t nonzero_mask(__m512i reg) {
+    return _mm512_cmpgt_epi32_mask(reg, _mm512_setzero_si512());
+}
+inline uint32_t nonzero_mask(__m256i reg) {
+    return _mm256_movemask_ps(
+        _mm256_castsi256_ps(_mm256_cmpgt_epi32(reg, _mm256_setzero_si256()))
+    );
+}
+inline uint32_t nonzero_mask(__m128i reg){
+    return _mm_movemask_ps(_mm_castsi128_ps(_mm_cmpgt_epi32(reg, _mm_setzero_si128())));
+}
 #endif
 
 simd<32> simdint_add(const simd<32>& a, const simd<32>& b);
@@ -137,18 +148,4 @@ simd<8> simd8_packus(const simd<16>& a, const simd<16>& b);
 simd<16> simd16_maddubs(const simd<8>& a, const simd<8>& b);
 simd<8> simd8_broadcast32(uint32_t v);
 simd<32> simdint_dpbusd(const simd<32>& acc, const simd<8>& u, const simd<8>& s);
-// AVX512: finds 16 nonzero blocks at a time
-inline uint32_t nonzero_mask(__m512i reg) {
-    return _mm512_cmpgt_epi32_mask(reg, _mm512_setzero_si512());
-}
-
-// AVX2: finds 8 nonzero blocks at a time
-inline uint32_t nonzero_mask(__m256i reg) {
-    return _mm256_movemask_ps(
-        _mm256_castsi256_ps(_mm256_cmpgt_epi32(reg, _mm256_setzero_si256()))
-    );
-}
-inline uint32_t nonzero_mask(__m128i reg){
-    return _mm_movemask_ps(_mm_castsi128_ps(_mm_cmpgt_epi32(reg, _mm_setzero_si128())));
-}
 #endif
